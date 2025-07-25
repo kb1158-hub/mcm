d15acccda749ea4ccde29cadb1abfca5a950f3f4
KarthicRaje <pakkakds5@gmail.com>
2025-07-25 10:06:13 +0200

    Update supabaseNotificationService.ts

--- src/services/supabaseNotificationService.ts
+++ src/services/supabaseNotificationService.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
-  process.env.REACT_APP_SUPABASE_URL!,
-  process.env.REACT_APP_SUPABASE_ANON_KEY!
+  process.env.VITE_APP_SUPABASE_URL!,
+  process.env.VITE_APP_SUPABASE_ANON_KEY!
 )
 
 export interface Notification {
